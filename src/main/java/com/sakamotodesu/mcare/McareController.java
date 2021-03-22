e373202de713d10e049b87ee5a9462d2af3c2d15
Minoru Sakamoto <p2020640@gmail.com>
2021-03-22 19:44:59 +0900

    dbおしゃべり

--- src/main/java/com/sakamotodesu/mcare/McareController.java
+++ src/main/java/com/sakamotodesu/mcare/McareController.java
@@ -1,10 +1,24 @@
 package com.sakamotodesu.mcare;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class McareController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @RequestMapping("/login")
     String login() {
@@ -17,7 +31,16 @@ public class McareController {
     }
 
     @RequestMapping("/reserve_yes")
-    String reserveYes() {
+    String reserveYes(Model model) {
+        jdbcTemplate.update("INSERT INTO reserve(reserve_date) VALUES('2000-01-01 10:00:00')");
+
+        List<String> reserveList = jdbcTemplate.query(
+                "SELECT id, reserve_date FROM reserve",
+                (rs, date) -> new Reserve(rs.getInt("id"),
+                        //rs.getString("reserve_date"))).forEach(reserve -> logger.info(reserve.toString()));
+                        rs.getString("reserve_date"))).stream().map(Reserve::getDate).collect(Collectors.toList());
+        reserveList.forEach(logger::info);
+        model.addAttribute("reserveList",reserveList);
         return "reserve_yes";
     }
 }
---
+++ src/main/java/com/sakamotodesu/mcare/Reserve.java
@@ -0,0 +1,12 @@
+package com.sakamotodesu.mcare;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Reserve {
+
+    private int id;
+    private String date;
+}
